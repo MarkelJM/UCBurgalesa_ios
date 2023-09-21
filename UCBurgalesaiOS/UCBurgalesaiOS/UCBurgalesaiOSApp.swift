cb2fa2058c57b9080f604b4379ef5e9762d7abd0
Markel <markjuaristi@gmail.com>
2023-09-21 12:52:50 +0200

    created the project

---
+++ UCBurgalesaiOS/UCBurgalesaiOS/UCBurgalesaiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  UCBurgalesaiOSApp.swift
+//  UCBurgalesaiOS
+//
+//  Created by Markel Juaristi on 21/9/23.
+//
+
+import SwiftUI
+
+@main
+struct UCBurgalesaiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
