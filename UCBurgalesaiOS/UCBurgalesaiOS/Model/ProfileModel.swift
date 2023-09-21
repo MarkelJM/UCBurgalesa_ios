079cf6b7e0792a1af892e6e29180b960e25841fc
Markel <markjuaristi@gmail.com>
2023-09-21 12:55:44 +0200

    added profileModel

---
+++ UCBurgalesaiOS/UCBurgalesaiOS/Model/ProfileModel.swift
@@ -0,0 +1,40 @@
+//
+//  ProfileModel.swift
+//  UCBurgalesaiOS
+//
+//  Created by Markel Juaristi on 21/9/23.
+//
+
+import Foundation
+
+struct ProfileModel {
+    var id: String
+    var clubId: String
+    var firstName: String
+    var lastName1: String
+    var lastName2: String
+    var bikeBrand: String
+    var address: Address
+    var phone: String
+    var email: String
+    var birthDate: Date
+    var federated: Bool
+    var volunteer: Bool
+    var rolesInClub: [String]
+    var profilePhoto: URL
+    var photos: [URL]
+    var routeType: RouteType
+    var facebookName: String
+    var stravaAccount: String
+}
+
+struct Address {
+    var postalCode: String
+    var city: String
+    var street: String
+}
+
+enum RouteType {
+    case short
+    case long
+}
