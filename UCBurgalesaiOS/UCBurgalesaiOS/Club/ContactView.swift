5c195080f9a306176e842a55a10e547ce02ae3de
Markel <markjuaristi@gmail.com>
2023-10-26 12:39:01 +0200

    added contactview info

--- UCBurgalesaiOS/UCBurgalesaiOS/Club/ContactView.swift
+++ UCBurgalesaiOS/UCBurgalesaiOS/Club/ContactView.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct ContactView: View {
     var body: some View {
-        Text("Contenido de Contacto")
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Para contactar personalmente con nosotros estamos en nuestra sede los Martes y Viernes de 20:30 a 21:30")
+                .padding(.bottom, 20)
+                .font(.title2)
+            
+            HStack {
+                Image(systemName: "house.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                Text("Unión Cicloturista Burgalesa")
+            }
+            
+            HStack {
+                Image(systemName: "mappin.and.ellipse")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                Text("C/ Pintor Manero Nº 7, 09006  Burgos")
+            }
+            
+            HStack {
+                Image(systemName: "phone.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                Text("Tel 609 785 799")
+            }
+            
+            HStack {
+                Image(systemName: "envelope.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                Text("E-Mail: informacion@ucburgalesa.com")
+            }
+        }
+        .padding()
     }
 }
 
