8114cfe1ad591747f55b4f1d6e7c7e615b6ccf61
Markel <markjuaristi@gmail.com>
2023-10-30 16:03:38 +0100

    newsmodel and extension for firebase done

--- UCBurgalesaiOS/UCBurgalesaiOS/Model/NewsModel/NewsModel+Firebase.swift
+++ UCBurgalesaiOS/UCBurgalesaiOS/Model/NewsModel/NewsModel+Firebase.swift
@@ -5,4 +5,30 @@
 //  Created by Markel Juaristi on 30/10/23.
 //
 
+import Foundation
 
+extension NewsModel {
+    
+    init?(from data: [String: Any]) {
+        guard let title = data["title"] as? String,
+              let content = data["content"] as? String,
+              let imageUrlString = data["imageUrl"] as? String,
+              let imageUrl = URL(string: imageUrlString) else {
+            return nil
+        }
+        
+        self.id = data["id"] as? String ?? UUID().uuidString
+        self.title = title
+        self.content = content
+        self.imageUrl = imageUrl
+    }
+    
+    func toFirestoreData() -> [String: Any] {
+        return [
+            "id": id,
+            "title": title,
+            "content": content,
+            "imageUrl": imageUrl.absoluteString
+        ]
+    }
+}
