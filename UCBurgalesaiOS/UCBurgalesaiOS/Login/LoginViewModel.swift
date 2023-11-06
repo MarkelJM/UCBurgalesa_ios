c0db0c1e7812e35e9e655dd6fdea58030455bcbd
Markel <markjuaristi@gmail.com>
2023-11-06 13:21:04 +0100

    completly added keychainmanager save and delete token

--- UCBurgalesaiOS/UCBurgalesaiOS/Login/LoginViewModel.swift
+++ UCBurgalesaiOS/UCBurgalesaiOS/Login/LoginViewModel.swift
@@ -9,12 +9,14 @@ import Foundation
 import FirebaseAuth
 import SwiftUI
 
+
 class LoginViewModel: ObservableObject {
     @Published var email: String = ""
     @Published var password: String = ""
     @Published var errorMessage: String = ""
     
     private var authManager = FirebaseAuthManager()
+    @StateObject private var keychainManager = KeychainManager()
     
     var appState: AppState
     init(appState: AppState) {
@@ -22,20 +24,19 @@ class LoginViewModel: ObservableObject {
     }
     
     func login() {
-        // Comprobar si el nombre de usuario y la contraseña son los especificados
-        if email == "Username" && password == "123456" {
-            appState.currentView = .home
-            return
-        }
-        
-        // Si no, continuar con la autenticación de Firebase
         authManager.login(email: email, password: password) { (success, error) in
             if let error = error {
                 self.errorMessage = error.localizedDescription
             } else if success {
-                // Navigate to the main app or dashboard
+                // Aquí debes obtener el token de Firebase y guardarlo
+                if let token = Auth.auth().currentUser?.uid { // Usando el UID como token de ejemplo
+                    self.keychainManager.saveToken(token) // Corregido aquí
+                }
+                self.appState.currentView = .home
             }
         }
     }
+
+    
 }
 
