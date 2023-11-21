6d5ab18a19db34482b4206c6f9c6449d2b5b4ff0
Markel <markjuaristi@gmail.com>
2023-11-21 19:29:40 +0100

    contactview design done

--- UCBurgalesaiOS/UCBurgalesaiOS/Club/ContactView.swift
+++ UCBurgalesaiOS/UCBurgalesaiOS/Club/ContactView.swift
@@ -4,26 +4,51 @@
 //
 //  Created by Markel Juaristi on 25/10/23.
 //
-
 import SwiftUI
 
 struct ContactView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Text("Para contactar personalmente con nosotros...")
-                .textStyle()
-            
-            Label("Unión Cicloturista Burgalesa", systemImage: "house.fill")
-                .labelStyle()
-            Label("C/ Pintor Manero Nº 7, 09006 Burgos", systemImage: "mappin.and.ellipse")
-                .labelStyle()
-            Label("Tel 609 785 799", systemImage: "phone.fill")
-                .labelStyle()
-            Label("E-Mail: informacion@ucburgalesa.com", systemImage: "envelope.fill")
-                .labelStyle()
+        ZStack {
+            DiagonalSolidShadedBackground()
+                .edgesIgnoringSafeArea(.all)
+
+            ScrollView {
+                
+                VStack(spacing: 40) {
+                    
+                    VStack(alignment: .leading, spacing: 15) {
+                        sectionHeader("Contacto")
+                            .padding(.horizontal, 100)
+                            
+                        
+                        Label("Unión Cicloturista Burgalesa", systemImage: "house.fill")
+                            .padding()
+                        Label("C/ Pintor Manero Nº 7, 09006 Burgos", systemImage: "mappin.and.ellipse")
+                            .padding()
+
+                        Label("Tel 609 785 799", systemImage: "phone.fill")
+                            .padding()
+
+                        Label("E-Mail: informacion@ucburgalesa.com", systemImage: "envelope.fill")
+                            .padding()
+
+                    }
+                    .sectionStyle()
+                }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 40)
+                .background(Color.white.opacity(0.9))
+                .cornerRadius(15)
+                .shadow(radius: 10)
+                .padding(.top, 150)
+            }
         }
-        .padding()
-        .background(DiagonalSolidShadedBackground())
+    }
+    
+    func sectionHeader(_ text: String) -> some View {
+        Text(text)
+            .font(.title2)
+            .padding(.bottom, 10)
     }
 }
 
