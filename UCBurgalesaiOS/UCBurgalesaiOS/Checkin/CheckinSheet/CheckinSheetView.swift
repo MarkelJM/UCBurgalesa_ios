5a4318a8feef6f09bf8edbfd8b1e951cb45ad4b8
Markel <markjuaristi@gmail.com>
2023-11-27 18:25:46 +0100

    checkin sheet design for start and rest done

--- UCBurgalesaiOS/UCBurgalesaiOS/Checkin/CheckinSheet/CheckinSheetView.swift
+++ UCBurgalesaiOS/UCBurgalesaiOS/Checkin/CheckinSheet/CheckinSheetView.swift
@@ -9,19 +9,44 @@ import SwiftUI
 
 struct CheckinSheetView: View {
     @ObservedObject var viewModel: CheckinViewModel
-    
+
     var body: some View {
-        VStack {
-            Button("Check-in Salida") {
-                viewModel.initiateCheckin(type: .start)
+        ZStack{
+            DiagonalSolidShadedBackground()
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 20) {
+                
+                
+                
+                Text("Check-in para la ruta")
+                    .font(.whatTheFont(size: 24))
+                    .padding()
+
+                Button(action: {
+                    viewModel.initiateCheckin(type: .start)
+                }) {
+                    Text("Check-in Salida")
+                        .font(.whatTheFont(size: 18))
+                }
+                .deepOrangeButton()
+                .frame(maxWidth: .infinity)
+
+                Button(action: {
+                    viewModel.initiateCheckin(type: .rest)
+                }) {
+                    Text("Check-in Descanso")
+                        .font(.whatTheFont(size: 18))
+                }
+                .brightOrangeButton()
+                .frame(maxWidth: .infinity)
             }
-            Button("Check-in Descanso") {
-                viewModel.initiateCheckin(type: .rest)
-            }
+            .padding()
+            .background(Color.white.opacity(0.9))
+            .cornerRadius(15)
+            .shadow(radius: 10)
         }
     }
 }
-
 
 struct CheckinSheetView_Previews: PreviewProvider {
     static var previews: some View {
