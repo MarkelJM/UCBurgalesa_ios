75d1bcefad453dbb22302db6f3c283c7c1d42924
Markel <markjuaristi@gmail.com>
2023-11-28 16:54:27 +0100

    primera version animacion onboarding done

--- UCBurgalesaiOS/UCBurgalesaiOS/Animations/LogoOnboarding/LogoAnimationView.swift
+++ UCBurgalesaiOS/UCBurgalesaiOS/Animations/LogoOnboarding/LogoAnimationView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct LogoAnimationView: View {
     @State private var imagePosition = CGPoint(x: 0, y: 0)
-    let animationDuration: Double
     let screenSize: CGSize
 
     var body: some View {
@@ -19,15 +18,22 @@ struct LogoAnimationView: View {
             .frame(width: 150, height: 150)
             .position(imagePosition)
             .onAppear {
-                withAnimation(.easeInOut(duration: animationDuration)) {
-                    imagePosition = CGPoint(x: screenSize.width / 2, y: screenSize.height / 2)
+                // Primera animación: Mover a la derecha
+                withAnimation(.easeInOut(duration: 1.0)) {
+                    imagePosition = CGPoint(x: screenSize.width + 75, y: screenSize.height / 2)
+                }
+                // Encadenar la siguiente animación después de un retardo
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    // Segunda animación: Mover al centro con efecto de rebote
+                    withAnimation(.timingCurve(0.68, -0.6, 0.32, 1.6, duration: 1.0)) {
+                        imagePosition = CGPoint(x: screenSize.width / 2, y: screenSize.height / 2)
+                    }
                 }
             }
     }
 
-    init(screenSize: CGSize, animationDuration: Double = 2.0) {
+    init(screenSize: CGSize) {
         self.screenSize = screenSize
-        self.animationDuration = animationDuration
-        _imagePosition = State(initialValue: CGPoint(x: -150, y: screenSize.height / 2))
+        _imagePosition = State(initialValue: CGPoint(x: -75, y: screenSize.height / 2))
     }
 }
