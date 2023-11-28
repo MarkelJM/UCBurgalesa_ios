3aa44c66be3657bc18e15e6762b0801b1b01a435
Markel <markjuaristi@gmail.com>
2023-11-28 18:14:53 +0100

    routes design done

--- UCBurgalesaiOS/UCBurgalesaiOS/Routes/RoutesListView.swift
+++ UCBurgalesaiOS/UCBurgalesaiOS/Routes/RoutesListView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Markel Juaristi on 20/10/23.
 //
-
 import SwiftUI
 
 struct RoutesListView: View {
@@ -12,37 +11,62 @@ struct RoutesListView: View {
     @EnvironmentObject var appState: AppState
     
     var body: some View {
-        VStack {
-            HStack {
-                BackButton(destination: .home)
-                    .environmentObject(appState)
-                Spacer()
-                
-            }
-            .padding()
-            
-            Text("Rutas")
-            List(viewModel.rides) { ride in
-                Button(action: {
-                    appState.currentView = .detailRoute
-                }) {
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(ride.rideName).font(.headline)
-                            Text(ride.organizer).font(.subheadline)
-                            Text(ride.restStopName).font(.subheadline)
-                            Text(DateFormatter.firestoreDateOnlyFormatter.string(from: ride.date)).font(.subheadline)
+        ZStack {
+            DiagonalSolidShadedBackground()
+                .edgesIgnoringSafeArea(.all)
+
+            VStack(spacing: 20) {
+                headerView
+
+                ScrollView {
+                    VStack(spacing: 20) {
+                        ForEach(viewModel.rides) { ride in
+                            Button(action: {
+                                appState.currentView = .detailRoute
+                            }) {
+                                rideCell(ride)
+                            }
+                            .padding()
+                            .background(Color.gray.opacity(0.1))
+                            .cornerRadius(10)
                         }
-                        Spacer()
-                        URLImage(url: ride.routeImage)
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 100, height: 100)
                     }
+                    .padding()
+                    .background(Color.white.opacity(0.9))
+                    .cornerRadius(15)
+                    .shadow(radius: 10)
                 }
             }
-            .onAppear(perform: viewModel.fetchRides)
         }
-        
+        .onAppear(perform: viewModel.fetchRides)
+    }
+
+    var headerView: some View {
+        HStack {
+            BackButton(destination: .home)
+                .environmentObject(appState)
+                .border(Color.white, width: 2)
+            Spacer()
+            Text("Rutas")
+                .font(.whatTheFont(size: 24))
+            Spacer()
+        }
+    }
+
+    func rideCell(_ ride: RideModel) -> some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(ride.rideName).font(.headline).foregroundColor(Color.violet5)
+                Text(ride.organizer).font(.subheadline).foregroundColor(Color.black)
+                Text(ride.restStopName).font(.subheadline).foregroundColor(Color.black)
+                Text(DateFormatter.firestoreDateOnlyFormatter.string(from: ride.date)).font(.subheadline).foregroundColor(Color.violet5)
+            }
+            Spacer()
+            URLImage(url: ride.routeImage)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 100)
+                .cornerRadius(10)
+        }
     }
 }
 
